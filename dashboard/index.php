b86c50473fd203e94580e2f5612ae15167c41574
Znth3 <zynthkas@gmail.com>
2020-10-28 06:35:14 +0700

    adding login feature

---
+++ dashboard/index.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+if(isset($_SESSION['no_rm'])){
+    $no_rm = $_SESSION['no_rm'];
+
+    if($no_rm == 'adminRS'){
+        header('location: admin');
+    }elseif($no_rm == null){
+        header('location: ../login');
+    }else{
+        header('location: user');
+    }
+}
