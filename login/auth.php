33d4d685950ebfcd26f83d7642e8cc9880515aad
Znth3 <zynthkas@gmail.com>
2020-10-28 06:32:21 +0700

    adding login feature

---
+++ login/auth.php
@@ -0,0 +1,25 @@
+<?php
+if (isset($_POST['masuk'])):
+    include "../connect.php";
+    $username = $_POST['username'];
+    $pass = md5($_POST['password']);
+
+    $sql = "SELECT no_rm FROM pasien WHERE username = '$username' AND password = '$pass'";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0):
+        session_start();
+
+        while ($row = $result->fetch_object()):
+            $_SESSION['no_rm'] = $row->no_rm;
+            header("location: ../dashboard");
+        endwhile;
+
+    else:
+        session_abort();
+
+    endif;
+
+endif;
+
